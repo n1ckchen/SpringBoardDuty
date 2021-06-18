9f69a0c3e6453e5d29713e2027bf5e2a7e7bb6dd
Nick <codingmaster@Nicks-MacBook-Air.local>
2021-06-18 10:44:14 -0400

    jasmine

--- Jasmine/helpers.js
+++ Jasmine/helpers.js
@@ -1,5 +1,5 @@
-
 // accepts 'tipAmt', 'billAmt', 'tipPercent' and sums total from allPayments objects
+/* // springboard solution, need further edit */
 function sumPaymentTotal(type) {
   let total = 0;
 
@@ -19,8 +19,28 @@ function calculateTipPercent(billAmt, tipAmt) {
 
 // expects a table row element, appends a newly created td element from the value
 function appendTd(tr, value) {
-  let newTd = document.createElement('td');
+  let newTd = document.createElement("td");
   newTd.innerText = value;
 
   tr.append(newTd);
+}
+
+// append delete button and click handler for removing server from allServers and DOM td
+function appendDeleteBtn(tr, type) {
+  let newTd = document.createElement("td");
+  newTd.className = "deleteBtn";
+  newTd.innerText = "X";
+
+  newTd.addEventListener("click", removeEle);
+
+  tr.append(newTd);
+}
+
+function removeEle(evt) {
+  let ele = evt.target.closest("tr");
+
+  delete allServers[ele.id];
+
+  ele.parentNode.removeChild(ele);
+  updateServerTable();
 }
--- Jasmine/payments.js
+++ Jasmine/payments.js
@@ -1,14 +1,15 @@
-let billAmtInput = document.getElementById('billAmt');
-let tipAmtInput = document.getElementById('tipAmt');
-let paymentForm = document.getElementById('paymentForm');
+/* // springboard solution, need further edit */
+let billAmtInput = document.getElementById("billAmt");
+let tipAmtInput = document.getElementById("tipAmt");
+let paymentForm = document.getElementById("paymentForm");
 
-let paymentTbody = document.querySelector('#paymentTable tbody');
-let summaryTds = document.querySelectorAll('#summaryTable tbody tr td');
+let paymentTbody = document.querySelector("#paymentTable tbody");
+let summaryTds = document.querySelectorAll("#summaryTable tbody tr td");
 
 let allPayments = {};
 let paymentId = 0;
 
-paymentForm.addEventListener('submit', submitPaymentInfo);
+paymentForm.addEventListener("submit", submitPaymentInfo);
 
 // Add a curPayment object to allPayments, update html and reset input values
 function submitPaymentInfo(evt) {
@@ -19,14 +20,14 @@ function submitPaymentInfo(evt) {
   if (curPayment) {
     paymentId += 1;
 
-    allPayments['payment' + paymentId] = curPayment;
+    allPayments["payment" + paymentId] = curPayment;
 
     appendPaymentTable(curPayment);
     updateServerTable();
     updateSummary();
 
-    billAmtInput.value = '';
-    tipAmtInput.value = '';
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
   }
 }
 
@@ -36,43 +37,37 @@ function createCurPayment() {
   let billAmt = billAmtInput.value;
   let tipAmt = tipAmtInput.value;
 
-  if (billAmt === '' || tipAmt === '') return;
+  if (billAmt === "" || tipAmt === "") return;
 
   if (Number(billAmt) > 0 && Number(tipAmt) >= 0) {
     return {
       billAmt: billAmt,
       tipAmt: tipAmt,
       tipPercent: calculateTipPercent(billAmt, tipAmt),
-    }
+    };
   }
 }
 
 // Create table row element and pass to appendTd with input value
 function appendPaymentTable(curPayment) {
-  let newTr = document.createElement('tr');
-  newTr.id = 'payment' + paymentId;
+  let newTr = document.createElement("tr");
+  newTr.id = "payment" + paymentId;
 
-  appendTd(newTr, '$' + curPayment.billAmt);
-  appendTd(newTr, '$' + curPayment.tipAmt);
-  appendTd(newTr, curPayment.tipPercent + '%');
+  appendTd(newTr, "$" + curPayment.billAmt);
+  appendTd(newTr, "$" + curPayment.tipAmt);
+  appendTd(newTr, "%" + curPayment.tipPercent);
+
+  appendDeleteBtn(newTr, "payment");
 
   paymentTbody.append(newTr);
 }
 
 // Create table row element and pass to appendTd with calculated sum of all payment
 function updateSummary() {
-  let tipPercentAvg;
-  let paymentTotal = sumPaymentTotal('tipPercent');
-  let numberOfPayments = Object.keys(allPayments).length;
+  let tipPercentAvg =
+    sumPaymentTotal("tipPercent") / Object.keys(allPayments).length;
 
-  if (paymentTotal === 0 && numberOfPayments === 0) {
-    tipPercentAvg = 0;
-  } else {
-    tipPercentAvg = paymentTotal / Object.keys(allPayments).length;
-  }
-
-  summaryTds[0].innerHTML = '$' + sumPaymentTotal('billAmt');
-  summaryTds[1].innerHTML = '$' + sumPaymentTotal('tipAmt');
-  summaryTds[2].innerHTML =  Math.round(tipPercentAvg) + '%';
+  summaryTds[0].innerHTML = "$" + sumPaymentTotal("billAmt");
+  summaryTds[1].innerHTML = "$" + sumPaymentTotal("tipAmt");
+  summaryTds[2].innerHTML = Math.round(tipPercentAvg) + "%";
 }
-
